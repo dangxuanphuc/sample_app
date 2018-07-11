532a2807e82bbd35dfad1330bddab143336f4dee
dangxuanphuc <dangxuanphuc2211@gmail.com>
2018-07-11 23:12:18 +0700

    Chapter 4: Rails-flavored Rubys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
-# frozen_string_literal: true
-
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
 end
