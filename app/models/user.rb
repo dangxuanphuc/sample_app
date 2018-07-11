7ec2cdab6e65fea7110aad0526b199682250b86c
dangxuanphuc <dangxuanphuc2211@gmail.com>
2018-07-11 23:12:18 +0700

    Chapter 9: Advanced Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,47 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  attr_reader :remember_token
+  before_save :email_downcase
+
   validates :name, presence: true,
     length: {maximum: Settings.name_max_length}
   validates :email, presence: true,
     length: {maximum: Settings.email_max_length},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true,
-    length: {minimum: Settings.password_min_length}
-
-  before_save :email_downcase
+    length: {minimum: Settings.password_min_length}, allow_nil: true
 
   has_secure_password
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token 
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
 
   private
 
